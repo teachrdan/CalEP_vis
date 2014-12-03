7b20f2a1643a78b5ecde7ceb24295db2b5fc379c
Greg Michalec <greg@primate.net>
2014-12-03 01:58:45 -0800

    cleaning up tooltips fixing districts view auto-reload auto-size label length fade when hovering on chord

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,20 @@
+var svg, info, tooltip, chord, height, width, labelSize; 
 var districts = [];
 var matrix = [];
 var districtIndex = {};
 var rows = [];
-var svg;
-var info;
-var tooltip;
-var width = 750;
-var height = 750;
-var chord;
-var labelSize = 100;
 var displayType = 'give';
+var maxLabel = "";
 
 var addDistrict = function(d) {
-  var displayType = $( "input:radio[name=display-type]:checked" ).val();
-  
   if (d) {
     d = $.trim(d);
+
     if (displayType == 'districts' && (d == 'Expert' || d == 'Neutral - CEP' || d == 'Neutral - WestEd')) { 
       return; 
     }
+    if (d.length > maxLabel.length) {maxLabel = d; } //Find longest label
+
     if (! districtIndex[d]) {
       districtIndex[d] = 1;
     }
@@ -26,7 +22,6 @@ var addDistrict = function(d) {
 }
 
 var createMap = function(data) {
-  var displayType = $( "input:radio[name=display-type]:checked" ).val();
   rows = data;
 
   $.each(data, function(i, row) {
@@ -42,6 +37,11 @@ var createMap = function(data) {
     }
   });
 
+  //set labelSize to 1/2 of longest label length
+  labelTest = svg.append('text').attr('id', 'maxLabel').text(maxLabel);
+  labelSize = $('#maxLabel').width()/2+2;
+  labelTest.remove();
+
   var x = 0;
   $.each(districtIndex, function(d) {
     districtIndex[d] = x;
@@ -53,37 +53,34 @@ var createMap = function(data) {
       index: x,
       name:d,
       relationships: [],
+      gives:0,
+      gets: 0
     }
     x++;
   })
 
-  var updateMatrix = function(from, to, rowIndex, columnIndex, count) {
-    if (displayType == 'give') {
-      to = districtIndex[columnIndex];
-    } else {
-      from = districtIndex[columnIndex];
-    }
+  var updateMatrix = function(from, to, rowIndex, count) {
     if (from == to) { return; } //Don't count self-relations
 
-    var minval = .1;
+    var minval = 0;
     var val = 1;
     if (displayType == 'districts' && count) {
-      
-      val = 1/count;
-      console.log(val);
+      //val = 1/count;
     }
 
     matrix[from][to]+= val + minval;
     if (displayType == 'both') {
-      matrix[to][from] = matrix[from][to];
+       matrix[to][from] += val + minval;
     } else {
-      matrix[to][from] = minval;      
+      matrix[to][from] += minval;      
     }
     if (districts[from].relationships[to] === undefined) { 
       districts[from].relationships[to] = [rowIndex];  
     } else {
       districts[from].relationships[to].push(rowIndex);
     }
+    districts[from].gives += val;
+    districts[to].gets += val;
   }
 
   $.each(data, function(rowIndex, row) {
@@ -98,29 +95,35 @@ var createMap = function(data) {
     }
 
     if (row.give && districtIndex[row.give] !== undefined) {
-      var from;
-      var to;
+      var from = districtIndex[row.give];
+      var to = districtIndex[row.get];
 
-      if (displayType == 'give') {
-        from = districtIndex[row.give];
-      } else {
+      if (displayType == 'get') {
         to = districtIndex[row.give];
       }
       $.each(gets, function(i, value){
-        console.log(gets.length)
-        updateMatrix(from, to, rowIndex, value, gets.length);        
+        if (displayType == 'get') {
+          from = districtIndex[value];
+        } else {
+          to = districtIndex[value];
+        }
+        updateMatrix(from, to, rowIndex, gets.length);        
       })
-    } else {
+    } else { //Process as a mutual give/get
       $.each(gets, function(i, get1){
         $.each(gets, function(i, get2){
           from = districtIndex[get1];
           to = districtIndex[get2];
-          updateMatrix(from, to, rowIndex, get1);  
+          if (displayType == 'get') {
+            from = districtIndex[get2];
+            to = districtIndex[get1];
+          }
+          updateMatrix(from, to, rowIndex, gets.length);  
         })
       })
     }
   });
-  // console.log(JSON.stringify(matrix).replace(/],/g, "],\n"));
+  console.log(JSON.stringify(matrix).replace(/],/g, "],\n"));
   drawGraph();
 }
 
@@ -191,6 +194,7 @@ var initGraph = function() {
 var redrawGraph = function() {
   displayType = $("input:radio[name=display-type]:checked").val();
   matrix = [];
+  maxLabel = "";
   districtIndex = {};
   districts = [];
   svg.selectAll("*").remove()
@@ -219,7 +223,13 @@ var drawGraph = function() {
   var fade = function(opacity) {
     return function(g, i) {
       svg.selectAll(".chords path")
-        .filter(function(d) { return d.source.index !== i && d.target.index !== i; })
+        .filter(function(d) { 
+          if (g.source) {
+            return d.source.index !== g.source.index || d.target.index !== g.target.index;
+          } else {
+            return d.source.index !== i && d.target.index !== i; 
+          }
+        })
         .transition()
         .style("opacity", opacity);
     };
@@ -227,12 +237,12 @@ var drawGraph = function() {
 
   chord = d3.layout.chord()
     .padding(0.02)
+    .sortChords( d3.descending ) 
+    .sortGroups(d3.descending)
     .sortSubgroups(d3.descending)
     .matrix(matrix);
 
   var fill = d3.scale.category20()
-    // .domain(d3.range(13))
-    // .range(d3.scale.category20);
 
   svg.append("g").attr('class', 'chord-groups').selectAll("path")
     .data(chord.groups)
@@ -242,7 +252,11 @@ var drawGraph = function() {
     .style("stroke", function(d) { return fill(d.index); })
     .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
     .on("mouseover", function(d, i) {
-      showTooltip(districts[i].name);
+      var tooltip = districts[i].name 
+        + "<br/>Gives: "+districts[i].gives
+        + "<br/>Gets: "+districts[i].gets;
+
+      showTooltip(tooltip);
       fade(0.1)(d, i);
     })
     .on("mouseout", fade(1))
@@ -285,32 +299,49 @@ var drawGraph = function() {
     })
     //Tooltip on chords shows 'From District X to District Y'
     .on("mouseover", function(d, i) {
-      var sourceDistIndex;
-      var targetDistIndex;
+      var sourceDistIndex =  d.source.index;
+      var targetDistIndex = d.target.index;
+      
       //These are reversed if the display is 'get-oriented'
-      if (displayType === 'get') {
-        sourceDistIndex = d.target.index;
+      if (displayType == 'get') {
+        sourceDistIndex =  d.target.index;
         targetDistIndex = d.source.index;
-        showTooltip(districts[sourceDistIndex].name 
-        + " to " + districts[targetDistIndex].name);
-      //In all other cases, it's a simple from-to relationship
-      } else {
-        sourceDistIndex = d.source.index;
-        targetDistIndex = d.target.index;
-        showTooltip(districts[sourceDistIndex].name 
-        + " to " + districts[targetDistIndex].name);
       }
+        
+      var tooltip = districts[sourceDistIndex].name
+        + " to " + districts[targetDistIndex].name + ": " + matrix[sourceDistIndex][targetDistIndex]
+        + "<br/>"+
+        districts[targetDistIndex].name 
+        + " to " + districts[sourceDistIndex].name + ": " + matrix[targetDistIndex][sourceDistIndex]
+      
+      showTooltip(tooltip);
+      fade(0.1)(d, i);
     })
+    .on("mouseout", fade(1))
+
 }
 
 $(function() {
-  var tabletop = Tabletop.init( { 
-    key: '1XkV1ePpq5piIfonZWShm7SZd78lqKvvgSP_u2hl54ic', 
-    callback: function(data) { 
-      initGraph();
-      createMap(data);
-    },
-    simpleSheet: true,
-    debug:true
-  } )
+  var tabletop;
+  var fetchData = function() {
+    tabletop = Tabletop.init( { 
+      key: '1XkV1ePpq5piIfonZWShm7SZd78lqKvvgSP_u2hl54ic', 
+      callback: function(data) { 
+        if (rows[0]) { return; }
+        initGraph();
+        rows = data;
+        createMap(data);
+      },
+      simpleSheet: true,
+      debug:true
+    } );
+  }
+
+  var checkData = function() {
+    if (rows[0]) { return; }
+    fetchData();
+    setTimeout(checkData, 3500);
+  }
+  
+  checkData();
 })
